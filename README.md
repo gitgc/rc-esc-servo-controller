f75f0071e2ddff602483aa496912a948b40ad925
Gio Coia <g@coia.io>
2025-07-22 13:14:36 -0700

    Get ESC controller code working on ESP32

---
+++ README.md
@@ -0,0 +1,5 @@
+# RC ESC Servo Controller
+
+This project is a simple RC ESC (Electronic Speed Controller) and steering servo controller using an ESP32.
+
+Note, this project borrows heavily from the [https://github.com/RB-ENantel/RC_ESC](https://github.com/RB-ENantel/RC_ESC) which provides ESC functionality for the Arduino platform.
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+#include <ESC.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN (2) // Built-in LED pin
+
+#define ESC_PIN (33) // ESC control pin
+
+#define ESC_ARM_VALUE (500) // Value to arm the ESC
+#define ESC_MIN_SPEED (1040) // speed low just low enough to not move the motor
+#define ESC_MAX_SPEED (1240) // maximum speed of the motor
+
+ESC rcESC (ESC_PIN, ESC_MIN_SPEED, ESC_MAX_SPEED, ESC_ARM_VALUE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Starting RC ESC Controller.");
+
+  // Configure ESC pins
+  pinMode(ESC_PIN, OUTPUT);
+  // Turn on the built-in LED
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Initialize the ESC
+  rcESC.arm();
+  Serial.println("ESC armed.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Tic");
+  delay(1000);
+  Serial.println("Tac");
+  delay(1000);
+  Serial.println("Toe");
+  delay(1000);
 }
