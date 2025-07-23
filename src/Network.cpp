b62644600c70f2b260d8ea5ea50e891c54f27fbc
Giovanni Coia <g@coia.io>
2025-07-22 18:48:28 -0700

    wifi code cleanup (#4)

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,38 +1,26 @@
 #include <Network.h>
 
-// Network class constructor
 Network::Network(const char* ssid, const char* password, int port) {
     this->ssid = ssid;
     this->password = password;
     this->port = port;
 }
 
-// Network class destructor
-Network::~Network() {
-    // Nothing to destruct
-}
+Network::~Network() { }
 
 void Network::connectToWiFi() {
-    Serial.print("Connecting to WiFi");
+    Serial.println("Connecting to WiFi...");
     WiFi.begin(this->ssid, this->password);
+    int wifiStatus = WiFi.waitForConnectResult(10000);
 
-    int maxAttempts = 40;
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < maxAttempts) {
-        delay(500);
-        Serial.print(".");
-        attempts++;
-    }
-
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("\nFailed to connect to WiFi :(");    
+    if (wifiStatus != WL_CONNECTED) {
+        Serial.println("Failed to connect to WiFi");
         // Using a blocking loop to prevent further execution
         while (true) {
             delay(1000);
         }
     }
-
-    Serial.println("\nConnected to WiFi!");
+    Serial.println("Connected to WiFi!");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 }
