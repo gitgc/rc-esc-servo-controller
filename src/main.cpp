2c91c696e3079bb2f01c477db715a3bbe057449c
Gio Coia <g@coia.io>
2025-07-22 18:52:02 -0700

    Remove unecessary port arg

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <ESC.h>
 #include <Network.h>
 
-
 const char* WIFI_SSID = "CHANGE_ME"; // Replace with your WiFi SSID
 const char* WIFI_PASSWORD = "CHANGE_ME"; // Replace with your WiFi password
 int LISTENING_PORT = 80; // Port for the web server
@@ -16,7 +15,7 @@ int LISTENING_PORT = 80; // Port for the web server
 #define ESC_MAX_SPEED (1240) // maximum speed of the motor
 
 ESC rcESC (ESC_PIN, ESC_MIN_SPEED, ESC_MAX_SPEED, ESC_ARM_VALUE);
-Network network(WIFI_SSID, WIFI_PASSWORD, LISTENING_PORT);
+Network network(WIFI_SSID, WIFI_PASSWORD);
 
 void setup() {
   Serial.begin(115200);
